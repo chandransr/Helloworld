aa0b12fb8e926b80231304193b490a81f6d41ce2
chandran.s.r <Chandran.S.R@cognizant.com>
2020-06-25 10:20:02 +0530

    code changes for sample

--- src/main/java/guru/springframework/SpringBootWebApplication.java
+++ src/main/java/guru/springframework/SpringBootWebApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class SpringBootWebApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootWebApplication.class, args);
+    public static void main(String[] arg) {
+        SpringApplication.run(SpringBootWebApplication.class, arg);
     }
 }
